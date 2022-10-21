6bec03659accefc6ca06987fa84681005049712f
tommijuslin <tommi.juslin@gmail.com>
2022-10-21 15:23:57 +0300

    Viikko 2. Tehtävä 3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,9 @@
 class Brewery < ApplicationRecord
     has_many :beers
+
+    def print_report
+        puts name
+        puts "established in year #{year}"
+        puts "number of beers #{beers.count}"
+    end
 end
