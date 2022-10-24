08605ded5bb8b02a3f3a57bdc41eb88fac4a5f43
tommijuslin <tommi.juslin@gmail.com>
2022-10-24 12:09:00 +0300

    Viikko 2. Tehtävä 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,10 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        total = 0
+        ratings.each { |rating| total += rating.score }
+        return total / ratings.count
+    end
 end
