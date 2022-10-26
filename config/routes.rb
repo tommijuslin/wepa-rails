2c161a44500865cc162085d3f399ce88ebaa7a70
tommijuslin <tommi.juslin@gmail.com>
2022-10-26 13:17:02 +0300

    Viikko 3. Tehtävä 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # get 'ratings', to: 'ratings#index'
-  # get 'ratings/new', to:'ratings#new'
-  # post 'ratings', to: 'ratings#create'
+  resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   root 'breweries#index'
 
-  resources :ratings, only: [:index, :new, :create, :destroy]
+  get 'signup', to: 'users#new'
 end
