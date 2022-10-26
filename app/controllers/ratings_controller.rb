bc48d5f20b2b1459ac3be33a526935c6b0d316e3
tommijuslin <tommi.juslin@gmail.com>
2022-10-26 15:31:05 +0300

    Viikko 3. Tehtävä 6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,15 +10,14 @@ class RatingsController < ApplicationController
 
   def create
     rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-    
-    redirect_to ratings_path
+    rating.user = current_user
+    rating.save
+    redirect_to current_user
   end
 
   def destroy
     rating = Rating.find(params[:id])
     rating.delete
-    redirect_to ratings_path
+    redirect_to current_user
   end
 end
