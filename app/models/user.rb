53ff6e3f552f02b2ab3719a6bda97e2c27540571
tommijuslin <tommi.juslin@gmail.com>
2022-11-03 11:50:09 +0200

    Viikko 4. Tehtävä 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,19 @@ class User < ApplicationRecord
   def to_s
     username.to_s
   end
+
+  def favorite_beer
+    return nil if ratings.empty?
+
+    ratings.order(score: :desc).limit(1).first.beer
+  end
+
+  def favorite_style
+    return nil if ratings.empty?
+
+    scores = ratings.group_by { |r| r.beer.style }
+                    .transform_values { |values| values.sum(&:score) / values.count.to_f }
+
+    scores.max_by(&:last).first
+  end
 end
