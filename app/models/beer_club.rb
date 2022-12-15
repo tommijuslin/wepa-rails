9741053ad3c980b2b55a7b965c03ca1e56ba238f
tommijuslin <tommi.juslin@gmail.com>
2022-12-15 14:35:03 +0200

    6-3

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -1,4 +1,8 @@
 class BeerClub < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :members, through: :memberships, source: :user
+
+  validates :name, presence: true
+  validates :founded, presence: true
+  validates :city, presence: true
 end
